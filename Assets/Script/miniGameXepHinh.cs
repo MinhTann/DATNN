ff274509c1ed8e60c16cea7858a573451c58dc79
ManhHung0508 <hungdo778894@gmail.com>
2025-02-14 02:15:52 +0700

    up

--- Assets/Script/miniGameXepHinh.cs
+++ Assets/Script/miniGameXepHinh.cs
@@ -5,68 +5,84 @@ using UnityEngine;
 public class miniGameXepHinh : MonoBehaviour
 {
     public GameObject[] Tetrominos;
-
     public float movementFrequency = 0.8f;
     private float passedTime = 0;
-
     private GameObject currentTetromino;
-    
+
+    // Start is called before the first frame update
     void Start()
     {
-        SpamTetromino();
+        SpawnTetromino();
     }
 
     // Update is called once per frame
     void Update()
     {
         passedTime += Time.deltaTime;
-        if(passedTime >= movementFrequency){
+        if (passedTime >= movementFrequency)
+        {
             passedTime -= movementFrequency;
             MoveTetromino(Vector3.down);
         }
         UserInput();
     }
-    void UserInput(){
-     if(Input.GetKeyDown(KeyCode.LeftArrow)){
-        MoveTetromino(Vector3.left);
-     }
-     else if(Input.GetKeyDown(KeyCode.RightArrow)){
-        MoveTetromino(Vector3.right);
-     }
-     else if(Input.GetKeyDown(KeyCode.UpArrow)){
-        currentTetromino.transform.Rotate(0, 0, 90);
-        if(!IsValidPosition()){
-            currentTetromino.transform.Rotate(0, 0 ,-90);
-        }
-     }
-     if(Input.GetKey(KeyCode.DownArrow)){
-        movementFrequency = 0.2f;
-     }else{
-        movementFrequency = 0.8f;
-     }
-    }
-    void SpamTetromino(){
-        int index = Random.Range(0, Tetrominos.Length);
-        currentTetromino = Instantiate(Tetrominos[index], new Vector3(5 , 18, 0), Quaternion.identity);
 
+    void UserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveTetromino(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveTetromino(Vector3.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            currentTetromino.transform.Rotate(0, 0, 90);
+            if (!IsValidPosition())
+            {
+                currentTetromino.transform.Rotate(0, 0, -90);
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movementFrequency = 0.2f;
+        }
+        else
+        {
+            movementFrequency = 0.8f;
+        }
     }
-    void MoveTetromino(Vector3 direction){
+
+    void SpawnTetromino()
+    {
+        int index = Random.Range(0, Tetrominos.Length);
+        currentTetromino = Instantiate(Tetrominos[index], new Vector3(5, 18, 0), Quaternion.identity);
+    }
+
+    void MoveTetromino(Vector3 direction)
+    {
         currentTetromino.transform.position += direction;
-        if(!IsValidPosition()){
+        if (!IsValidPosition())
+        {
             currentTetromino.transform.position -= direction;
-            if(direction == Vector3.down){
-               
+            if (direction == Vector3.down)
+            {
+                GetComponent<Gird>().UpdateGird(currentTetromino.transform);
                 CheckForLines();
-                SpamTetromino();
+                SpawnTetromino();
             }
         }
     }
 
-    bool IsValidPosition(){
-       return GetComponent<Gird>().IsValidPosition(currentTetromino.transform);
+    bool IsValidPosition()
+    {
+        return GetComponent<Gird>().IsValidPosition(currentTetromino.transform);
     }
 
-    void CheckForLines(){
-         GetComponent<Gird>().CheckForLines();
+    void CheckForLines()
+    {
+        GetComponent<Gird>().CheckForLines();
     }
 }
