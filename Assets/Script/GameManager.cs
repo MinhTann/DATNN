4f60f840b534726f322de8ede93ce486b6b90ec2
ManhHung0508 <hungdo778894@gmail.com>
2025-02-14 01:55:22 +0700

    miniXH

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
 
     public GameObject WinGame;
     private void Awake(){
-        card = Resources.LoadAll<Sprite>("Ảnh/Animals");
+        card = Resources.LoadAll<Sprite>("Ảnh/AnhCard");
     }
     // Start is called before the first frame update
     void Start()
