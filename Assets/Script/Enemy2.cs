2d6954dc84818eb599f6b9633d605547a2ce17ff
MinhTann <94956703+MinhTann@users.noreply.github.com>
2025-02-19 07:52:20 +0700

    healthbar hoan thanh

--- Assets/Script/Enemy2.cs
+++ Assets/Script/Enemy2.cs
@@ -5,53 +5,67 @@ using UnityEngine.UIElements;
 
 public class Enemy2 : MonoBehaviour
 {
-    [SerializeField] private float attackCooldown;
+    [SerializeField] private float attackCooldown = 10f;
     [SerializeField] private int damage;
     [SerializeField] private float Range;
+     [SerializeField] private float colliderDistance;
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
     private Animator animator;
-    private Player player;
+
+    private Health playerHealth;
     private float cooldownTimer = Mathf.Infinity;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();    
-        player = GetComponent<Player>();
+        animator = GetComponent<Animator>();
+      
     }
-
     // Update is called once per frame
     void Update()
     {
         cooldownTimer += Time.deltaTime;
-        if(PlayerinSight())
+        if (PlayerinSight())
         {
-            
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
                 animator.SetTrigger("Attack");
+                
             }
         }
 
-       
+
     }
     private bool PlayerinSight()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * Range * transform.localScale.x, boxCollider.bounds.size, 0, Vector2.left, 0, playerLayer);
-        
-        
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * Range * transform.localScale.x * colliderDistance, 
+            new Vector3(boxCollider.bounds.size.x * Range, boxCollider.bounds.size.y, boxCollider.bounds.size.z)
+            , 0, Vector2.left, 0, playerLayer);
+        if (hit.collider != null)
+        {
+            playerHealth = hit.transform.GetComponent<Health>();
+        }
         return hit.collider != null;
+        
     }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * Range * transform.localScale.x, boxCollider.bounds.size);
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * Range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * Range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
     public void TakeDame()
     {
         animator.SetTrigger("Hit");
 
+    }
+    private void DamePlayer()
+    {
+        if (PlayerinSight())
+        {
+           playerHealth.TakeDame(damage);
+        }
     }
 }
---
+++ Assets/Script/Health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float startingHealth;
+
+    public float currenthealth {get; set;}
+    void Start()
+    {
+        currenthealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            TakeDame(1);
+        }
+    }
+    public void TakeDame(float dame)
+    {
+        currenthealth = Mathf.Clamp(currenthealth - dame, 0, startingHealth);
+
+        if (currenthealth > 0)
+        {
+            //Hurt
+        }
+        else
+        {
+            //Die
+        }
+    }
+}
