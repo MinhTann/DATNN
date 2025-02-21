3136de29e74dc374226e617eda4c447835a44cb2
nguyenluminhhao123 <nguyenluminhhao1012020@gmail.com>
2025-02-21 17:03:42 +0700

    addMiniGame2

---
+++ Assets/ps31623_NLMH/MiniGame2/GameManager2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager2  : MonoBehaviour
+{
+    public GameObject[] goals;
+    public GameObject[] boxes;
+
+    void Start()
+    {
+        goals = GameObject.FindGameObjectsWithTag("Goal");
+        boxes = GameObject.FindGameObjectsWithTag("Box");
+    }
+
+    void Update()
+    {
+        Winner();
+    }
+
+    bool CheckWinCondition()
+    {
+        foreach (GameObject goal in goals)
+        {
+            bool boxOnGoal = false;
+            foreach (GameObject box in boxes)
+            {
+                if (Vector2.Distance(goal.transform.position, box.transform.position) < 0.1f)
+                {
+                    boxOnGoal = true;
+                    break;
+                }
+            }
+            if (!boxOnGoal) return false;
+        }
+        return true;
+    }
+
+    public void Winner()
+    {
+
+        if (CheckWinCondition())
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
+
---
+++ Assets/ps31623_NLMH/MiniGame2/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public LayerMask wallLayer;
+    public LayerMask boxLayer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) Move(Vector2.up);      
+        if (Input.GetKeyDown(KeyCode.S)) Move(Vector2.down);    
+        if (Input.GetKeyDown(KeyCode.A)) Move(Vector2.left);    
+        if (Input.GetKeyDown(KeyCode.D)) Move(Vector2.right);   
+    }
+
+    void Move(Vector2 direction)
+    {
+        Vector2 newPosition = (Vector2)transform.position + direction;
+
+        // Kiểm tra tường
+        if (Physics2D.OverlapCircle(newPosition, 0.1f, wallLayer))
+
+            return;
+
+        Collider2D box = Physics2D.OverlapCircle(newPosition, 0.1f, boxLayer);
+        if (box != null)
+        {
+            Vector2 boxNewPosition = newPosition + direction;
+            if (Physics2D.OverlapCircle(boxNewPosition, 0.1f, wallLayer) ||
+                Physics2D.OverlapCircle(boxNewPosition, 0.1f, boxLayer))
+            {
+                return;
+            }
+
+            box.transform.position = boxNewPosition; 
+        }
+
+        transform.position = newPosition;
+    }
+}
+
