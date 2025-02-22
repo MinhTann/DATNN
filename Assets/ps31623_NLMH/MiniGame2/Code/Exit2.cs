e0bd320428e22015b316e88fc9e2ba97af74c4ef
nguyenluminhhao123 <nguyenluminhhao1012020@gmail.com>
2025-02-22 09:55:32 +0700

    comit

---
+++ Assets/ps31623_NLMH/MiniGame2/Code/Exit2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit2 : MonoBehaviour
+{
+    public void Exit()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+}
+
