e9f08833c39383cfe279dbfa85d6f67807b45cae
nguyenluminhhao123 <nguyenluminhhao1012020@gmail.com>
2025-02-22 18:19:20 +0700

    fixBug

--- Assets/ps31623_NLMH/MiniGame2/Code/ButtonExit.cs
+++ Assets/ps31623_NLMH/MiniGame2/Code/ButtonExit.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using System.Collections;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Exit2 : MonoBehaviour
+public class ButtonExit : MonoBehaviour
 {
     public void Exit()
     {
         SceneManager.LoadScene("Exit");
     }
 }
-
