499c3ff86127da0f77304e76415ad92e0b0c3a8a
nguyenluminhhao123 <nguyenluminhhao1012020@gmail.com>
2025-02-24 08:22:00 +0700

    updateMG2

---
+++ Assets/ps31623_NLMH/MiniGame2/Code/Next.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next : MonoBehaviour
+{
+    public void NextMG2_2()
+    {
+        SceneManager.LoadScene("MG2_2");
+    }
+}
